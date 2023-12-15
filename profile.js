833c5e142560b17dd18955f4e7ea95e9c6392df4
Maheshwaran <tmmaheshwaran@gmail.com>
2023-12-16 00:41:30 +0530

    added email form validations

--- profile.js
+++ profile.js
@@ -52,17 +52,54 @@ function navOnClick() {
   }
 }
 
-function sendEmail() {
-  const fromMail = document.querySelector("#mail").value;
-  const name = document.querySelector("#name").value;
-  const messageBody = document.querySelector("#message").value;
-  const formatedMsg = `<p>${messageBody}</p></br><span>Regards,<span></br><b>${name}</b>`;
-  console.log(fromMail, name, formatedMsg);
-  Email.send({
-    SecureToken: "fa6ce737-d3bd-46fa-b464-b4416fd6d375",
-    To: "tmmaheshwaran@gmail.com",
-    From: fromMail,
-    Subject: "Query from portfolio",
-    Body: formatedMsg,
-  }).then((message) => alert(message));
+async function sendEmail() {
+  try {
+    const fromMailField = document.querySelector("#mail");
+    const nameField = document.querySelector("#name");
+    const messageBodyField = document.querySelector("#message");
+    const fromMail = fromMailField.value;
+    const name = nameField.value;
+    const messageBody = messageBodyField.value;
+    const formatedMsg = `<p>${messageBody}</p></br><span>Regards,<span></br><b>${name}</b>`;
+    const msg = document.querySelector("#msg-success");
+    if (name === "" || fromMail === "" || messageBody === "") {
+      let message = "";
+      if (name === "") {
+        message = `Please Enter Your Name, `;
+        nameField.classList.add("input-error");
+      } else {
+        nameField.classList.remove("input-error");
+      }
+      if (fromMail === "") {
+        message += `Please Enter Your Mail, `;
+        fromMailField.classList.add("input-error");
+      } else {
+        fromMailField.classList.remove("input-error");
+      }
+      if (messageBody === "") {
+        message+= `Please Enter a Message`;
+        messageBodyField.classList.add("input-error");
+      } else {
+        messageBodyField.classList.remove("input-error");
+      }
+      msg.innerHTML = message;
+      return;
+    }
+    nameField.classList.remove("input-error");
+    fromMailField.classList.remove("input-error");
+    messageBodyField.classList.remove("input-error");
+
+    await Email.send({
+      SecureToken: "fa6ce737-d3bd-46fa-b464-b4416fd6d375",
+      To: "tmmaheshwaran@gmail.com",
+      From: fromMail,
+      Subject: "Query from portfolio",
+      Body: formatedMsg,
+    }).then(() => {
+      msg.innerHTML = `Thanks for reaching out me <b>${name}</b>!. I will get back to you as soon as I can!!`;
+      return;
+    });
+  } catch (err) {
+    msg.innerHTML = `Try again later! Thanks`;
+  }
 }
